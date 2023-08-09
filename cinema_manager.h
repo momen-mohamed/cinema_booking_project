1490146074d8b35cbdbc145799374366d9aa7b28
Mahmoud Ahmed <88000367+MahmoudKR@users.noreply.github.com>
2023-08-09 22:22:45 +0300

    Update cinema_manager.h
    1- Included "MyP_Types.h
    2- Replaced "int" -> "Int32_t"
    3- Replaced "char" -> "Int8_t"

--- cinema_manager.h
+++ cinema_manager.h
@@ -1,6 +1,7 @@
 
 #ifndef CINMEA_MANAGER_H_
 #define CINMEA_MANAGER_H_
+#include "MyP_Types.h"
 
 void start_app();
 void select_user();
@@ -10,9 +11,9 @@ void manage_showing_ticket();
 void manage_ticket_cancel();
 void manage_admin_selection();
 void manage_change_price();
-void get_pass(char *pass);
-void manage_cancelBySeat(int selected_movie);
-void manage_cancelByPhoneNumber(int selected_movie);
+void get_pass(Int8_t *pass);
+void manage_cancelBySeat(Int32_t selected_movie);
+void manage_cancelByPhoneNumber(Int32_t selected_movie);
 
 
 #endif
